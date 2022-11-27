6a9fb2a4aac4e3390b8a8e5b25fab5574bb1ee06
Haley Brightwell <hbrightwell1101@gmail.com>
2022-11-26 18:00:00 -0600

    Add all files

---
+++ src/ErrorProjectEx/ErrorProjectHW.java
@@ -0,0 +1,33 @@
+/* Create a small program that defines some fields. Try creating some illegal 
+ field names and see what kind of error the compiler produces. Use the naming 
+ rules and conventions as a guide. In the program you created, try leaving the fields 
+ uninitialized and print out their values. Try the same with a local variable and see 
+ what kind of compiler errors you can produce. Becoming familiar with common compiler 
+ errors will make it easier to recognize bugs in your code. */
+
+
+
+package ErrorProjectEx;
+//illegal field names
+public class ErrorProjectHW {
+int num;
+int bananas;
+int int;
+int 1;
+int 67;
+int .;
+
+
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(bananas);
+		System.out.println(int);
+		System.out.println(num);
+		
+		
+		
+	}
+	
+}
